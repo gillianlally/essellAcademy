c14ddfe76a99b29f33dfbff9c7802973f90c73a8
Guest User <Guest@stk-cs01-ws05.westlothian.sch.uk>
2015-12-15 10:23:12 +0000

    sprint1.1

---
+++ TICKETORDER.java
@@ -0,0 +1,17 @@
+
+import javax.swing.JOptionPane;    
+
+//ticket order = pupil
+//purchase = mark
+// topmark = purchasemethod
+public class TICKETORDER 
+{ 
+    private String customerID ;
+    private char ticketID ;
+    private int noOfTickets ;
+    private String methodOfPurchase ;
+    public TICKETORDER ()
+    { 
+        //constructor
+    }
+}
