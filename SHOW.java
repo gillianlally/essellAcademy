a29711c76bf26fde3211d45445ae03326bd2720a
Guest User <Guest@stk-cs01-ws05.westlothian.sch.uk>
2015-12-16 12:29:18 +0000

    2.1

--- SHOW.java
+++ SHOW.java
@@ -3,9 +3,39 @@ import java.io.* ;
 
 public class SHOW 
 {
+    // array
+
+    private TICKETORDER ticketorderList [] ;
+    private int noOfTicketsOrdered ; 
+    private int purchaseMethodW ;
+    private int purchaseMethodS ;
+    private String mostPopular ;
+    private int Total ;
     
+    FILEREADCSV purchaseFile ;
+    
+    FILEWRITECSV resultsFile ; 
+
     public SHOW() throws IOException
     {  
-        // 
+        purchaseMethodFile = new FILEREADCSV () ;
+        resultsFile = new FILEWRITECSV () ;
+        Total = 0 ;
+        purchaseMethodW = 0 ;
+        purchaseMethodS = 0 ;
+        noOfTicketsOrdered = 0 ;
+        mostPopular = "" ;
+    }
+
+    public void processticketOrder () 
+    {
+        setUpticketOrderList () ;
+        
+
+    }
+
+    public void setUpticketorder () 
+    {
+        //
     }
 }
