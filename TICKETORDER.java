09d68621f72b9a288a301eb389ea649cafa8e745
Guest User <Guest@stk-cs01-ws05.westlothian.sch.uk>
2015-12-16 11:25:58 +0000

    sprint1.3
    finished sprint 1

--- TICKETORDER.java
+++ TICKETORDER.java
@@ -19,4 +19,38 @@ public class TICKETORDER
         noOfTickets = 0 ;
         methodOfPurchase = "" ;
     }
+
+    public void readTICKETORDERDetails(String dataItems)
+    {
+        //unpack string of row data into fields
+        String[] rowItems = dataItems.split(",") ;
+
+        // store each data item as instance property
+        customerID = rowItems[0] ;
+        tID = (rowItems[1]).charAt(0) ; 
+        ticketID = rowItems [1] ;
+        noOfTickets =  Integer.parseInt(rowItems[2]) ;
+        methodOfPurchase = rowItems[3] ; 
+    }
+
+    public String writeDetails()
+    {
+        // join up data into a string to output as a row
+        // use "," to separate csv columns
+        String memberData = "";
+        memberData = memberData.concat(customerID);
+        memberData = memberData.concat(",");
+        memberData = memberData.concat(ticketID);
+        memberData = memberData.concat(",");
+        memberData = memberData.concat(Float.toString(noOfTickets));
+        memberData = memberData.concat(",") ;
+        memberData = memberData.concat(methodOfPurchase) ;
+        return memberData;
+    }
+
+    public String getmethodOfPurchase ()
+    {
+        return methodOfPurchase ;
+    }
+
 }
